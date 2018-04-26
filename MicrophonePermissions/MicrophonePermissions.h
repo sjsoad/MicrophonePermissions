af7e6cea2945f2c3b436053145d48913a706be64
Sergey Kostyan <>
2018-04-26 22:24:56 +0300

    move to open

---
+++ MicrophonePermissions/MicrophonePermissions.h
@@ -0,0 +1,19 @@
+//
+//  MicrophonePermissions.h
+//  MicrophonePermissions
+//
+//  Created by Sergey Kostyan on 24.04.2018.
+//  Copyright © 2018 Sergey Kostyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MicrophonePermissions.
+FOUNDATION_EXPORT double MicrophonePermissionsVersionNumber;
+
+//! Project version string for MicrophonePermissions.
+FOUNDATION_EXPORT const unsigned char MicrophonePermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicrophonePermissions/PublicHeader.h>
+
+
---
+++ MicrophonePermissions/Sources/MicrophonePermissions.swift
@@ -0,0 +1,35 @@
+//
+//  MicrophonePermissions.swift
+//  SKUtilsSwift
+//
+//  Created by Sergey Kostyan on 01.10.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import SKServicePermissions
+
+open class MicrophonePermissions: NSObject {
+    
+}
+
+// MARK: - ServicePermissions -
+
+extension MicrophonePermissions: ServicePermissions {
+    
+    public typealias PermissionsState = AVAudioSessionRecordPermission
+    
+    public func requestPermissions(handler: @escaping (PermissionsState) -> Void) {
+        AVAudioSession.sharedInstance().requestRecordPermission { [weak self] _ in
+            guard let strongSelf = self else { return }
+            handler(strongSelf.permissionsState())
+        }
+    }
+    
+    public func permissionsState() -> PermissionsState {
+        return AVAudioSession.sharedInstance().recordPermission()
+    }
+
+}
+
